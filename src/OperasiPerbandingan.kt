3cc59a592659bef99f68c772e28890c85e3b039a
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-04-06 21:12:50 +0700

    operasi perbandingan

---
+++ src/OperasiPerbandingan.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val a = 100
+    val b = 200
+
+    val result: Boolean = a < b
+    println(result)
+
+    println(100 >= 100)
+    println(100 <= 100)
+
+    println("hanif">="hanif")
+    println("a"<"b")
+    println("a"=="a")
+    println("a"!="a")
+}
