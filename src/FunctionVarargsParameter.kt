b1df5532fc6c1e21ccb8777440c04befa4d2fe89
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-04-06 22:57:42 +0700

    function varargs parameter

---
+++ src/FunctionVarargsParameter.kt
@@ -0,0 +1,13 @@
+fun hitungTotal(name:String, vararg values:Int):Int{
+    var total = 0
+
+    for (value in values){
+        total+=value
+    }
+    return total
+}
+
+fun main() {
+    val result = hitungTotal("hanif", 10, 10,10)
+    println(result)
+}
