ab0b06593fcfa20424846e246d8d2749d38663a7
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-04-06 21:20:35 +0700

    if expression

---
+++ src/IfExpression.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val nilai = 90
+
+    if (nilai > 90){
+        println("Cool")
+    } else if (nilai > 75){
+        println("Good Job")
+    } else if (nilai > 60){
+        println("Not Bad")
+    } else{
+        println("Silakan coba lagi")
+    }
+
+}
