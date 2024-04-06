753ada4829b6f76a2508196a635afa4ae3abbedb
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-04-07 02:24:06 +0700

    tail recursive function

---
+++ src/TailRecursiveFunction.kt
@@ -0,0 +1,10 @@
+fun main() {
+    tailrec fun display(value: Int){
+        println("Recursive $value")
+        if (value > 0){
+            display(value - 1)
+        }
+    }
+
+    display(10000)
+}
