4fe83c77ba170d0bc48617971b52a1a523f050a0
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-04-07 02:16:51 +0700

    recursive function

---
+++ src/RecursiveFunction.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+    fun factorialloop(value:Int): Int{
+        var result = 1
+        for(i in value downTo 1){
+            result *= i
+        }
+        return result
+    }
+
+    fun factorialRecursive(value: Int):Int{
+        return when (value){
+            1 -> 1
+            else -> value * factorialRecursive(value - 1)
+        }
+    }
+
+    println(factorialloop(10))
+    println(factorialRecursive(10))
+
+}
